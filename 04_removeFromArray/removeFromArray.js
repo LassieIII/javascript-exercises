2fe034af356b552b3ba4c59cae61e4f5291fcf27
Richard <45016368+LassieIII@users.noreply.github.com>
2023-06-23 12:32:20 -0400

    function created to remove an unlisted number of elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const mid = ["BuzzzOS","Buzzz","Comet"];
+const removeFromArray = function(array, ...args){
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+          }
+    });
+    return newArray;
+};
+
+removeFromArray([1,2,3,4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
